33082c071b843955db04fb17e01650a196b6b863
LuisToscano74. <[200~163039949+LuisToscano74@users.noreply.github.com~>
2024-09-11 13:06:02 -0600

    Comentarios y variables en Javascript

---
+++ 01 - Fundamentos/comentarios.js
@@ -0,0 +1,18 @@
+// Comentarios 
+
+/* 
+Comentarios en bloque:
+
+Para más es una linea de comemtarios.
+Usamos el shortcut shift + alt + a
+*/
+
+// La función alert() lanza una ventana de alerta.
+// Verificamos enlace index.html (Mejor ejemplo).
+alert ("Estoy enlazado!!");
+
+/* Recomendación
+
+1. No debemos recargar el código de comentarios.
+2. Los comentarios deben ser concretos y descriptivos.
+3. No deben de ser redundantes respecto al código.*/
